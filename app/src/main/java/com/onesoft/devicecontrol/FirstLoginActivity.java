cc201cec0ae9d10b66043ef7f26206a1b214c43c
Jozef Lovas <Jozef Lovas>
2016-10-05 23:35:32 +0200

    This is first commit

---
+++ app/src/main/java/com/onesoft/devicecontrol/FirstLoginActivity.java
@@ -0,0 +1,13 @@
+package com.onesoft.devicecontrol;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FirstLoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fyrst_login);
+    }
+}

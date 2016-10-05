00d3b031901822fb332589c30dc6af46d32a895b
exosj <exosjl@gmail.com>
2016-10-05 23:44:00 +0200

    Testovanie commitu

---
+++ app/src/main/java/com/onesoft/devicecontrol/login.java
@@ -0,0 +1,8 @@
+package com.onesoft.devicecontrol;
+
+/**
+ * Created by exosj on 05.10.2016.
+ */
+
+public class login {
+}
---
+++ app/src/test/java/com/onesoft/devicecontrol/loginTest.java
@@ -0,0 +1,10 @@
+package com.onesoft.devicecontrol;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by exosj on 05.10.2016.
+ */
+public class loginTest {
+
+}
